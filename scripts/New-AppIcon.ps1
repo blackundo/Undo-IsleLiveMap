d05d8fa4b19829077b43d093c84e89507792dfee
Black Undo <81552488+blackundo@users.noreply.github.com>
2026-09-17 06:46:41 +0700

    chore: update app icon

--- scripts/New-AppIcon.ps1
+++ scripts/New-AppIcon.ps1
@@ -18,29 +18,29 @@ $outputDirectory = Split-Path -Parent $resolvedOutput
 $bitmap = New-Object System.Drawing.Bitmap 256,256
 $graphics = [System.Drawing.Graphics]::FromImage($bitmap)
 $graphics.SmoothingMode = [System.Drawing.Drawing2D.SmoothingMode]::AntiAlias
-$graphics.Clear([System.Drawing.Color]::FromArgb(7,23,22))
+$graphics.Clear([System.Drawing.Color]::FromArgb(20,10,36))
 
-$cyan = [System.Drawing.Color]::FromArgb(55,212,198)
-$bone = [System.Drawing.Color]::FromArgb(233,244,238)
-$amber = [System.Drawing.Color]::FromArgb(231,183,78)
-$linePen = New-Object System.Drawing.Pen ([System.Drawing.Color]::FromArgb(80,55,212,198)),6
-$innerPen = New-Object System.Drawing.Pen ([System.Drawing.Color]::FromArgb(130,55,212,198)),3
+$purple = [System.Drawing.Color]::FromArgb(183,92,255)
+$lavender = [System.Drawing.Color]::FromArgb(246,236,255)
+$magenta = [System.Drawing.Color]::FromArgb(239,92,196)
+$linePen = New-Object System.Drawing.Pen ([System.Drawing.Color]::FromArgb(110,183,92,255)),6
+$innerPen = New-Object System.Drawing.Pen ([System.Drawing.Color]::FromArgb(165,183,92,255)),3
 $graphics.DrawEllipse($linePen,28,28,200,200)
 $graphics.DrawEllipse($innerPen,63,63,130,130)
 
 $arrow = New-Object System.Drawing.Drawing2D.GraphicsPath
 $arrow.AddPolygon([System.Drawing.Point[]]@(
-    [System.Drawing.Point]::new(128,36),
-    [System.Drawing.Point]::new(172,161),
-    [System.Drawing.Point]::new(128,137),
-    [System.Drawing.Point]::new(84,161)
+    [System.Drawing.Point]::new(220,128),
+    [System.Drawing.Point]::new(95,172),
+    [System.Drawing.Point]::new(119,128),
+    [System.Drawing.Point]::new(95,84)
 ))
-$arrowBrush = New-Object System.Drawing.SolidBrush $bone
-$arrowPen = New-Object System.Drawing.Pen $cyan,5
+$arrowBrush = New-Object System.Drawing.SolidBrush $lavender
+$arrowPen = New-Object System.Drawing.Pen $purple,5
 $graphics.FillPath($arrowBrush,$arrow)
 $graphics.DrawPath($arrowPen,$arrow)
 
-$centerBrush = New-Object System.Drawing.SolidBrush $amber
+$centerBrush = New-Object System.Drawing.SolidBrush $magenta
 $graphics.FillEllipse($centerBrush,116,116,24,24)
 
 $iconHandle = $bitmap.GetHicon()
